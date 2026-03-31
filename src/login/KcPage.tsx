525a93c144885d653dc78ed848020f13fac9c143
dcarboMC <dcarbo@market-control.com>
2026-03-31 14:52:41 +0200

    changed title name

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -1,9 +1,13 @@
 import { Suspense, lazy } from "react";
+import { useEffect } from "react";
 import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
+// import main.css
+import "./main.css";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
+
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
@@ -14,6 +18,16 @@ export default function KcPage(props: { kcContext: KcContext }) {
     const { kcContext } = props;
 
     const { i18n } = useI18n({ kcContext });
+
+    // Set title name
+    useEffect(() => {
+        console.log("SETTING TITLE2!");
+        const interval = setInterval(() => {
+            document.title = "Log in to NoOrg";
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <Suspense>
